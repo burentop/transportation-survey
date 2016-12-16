0a6f93c1dc6a9ba38d7fb2577d17eebfb1f1503a
Brent Perry <burentoperii@gmail.com>
2016-12-16 09:42:29 -0600

    complete logic to gather checkbox values.

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,12 @@ $(document).ready(function () {
     $("#work-responses").show();
     $("input:checkbox[name=work-transportation]:checked").each(function () {
       var workTransportationMode = $(this).val();
-      $('#work-responses').append("<br>" + workTransportationMode);
+      $('#work-responses').append(workTransportationMode + "<br>");
+    });
+    $("#fun-responses").show();
+    $("input:checkbox[name=fun-transportation]:checked").each(function () {
+      var funTransportationMode = $(this).val();
+      $("#fun-responses").append(funTransportationMode + "<br>");
     });
     $('#transportation_survey').hide();
   });
